c670eb1c0a0aedf6056a57ad4e07111b63c86e09
Paul Laskowski <pelaskowski@gmail.com>
2014-10-01 21:22:53 -0700

    integrating templates and refactoring js code to render a one to many type join between an organization in it's locations.

--- client/ui.js
+++ client/ui.js
@@ -5,22 +5,21 @@
 		menuLink = document.getElementById('menuLink');
 
 	function toggleClass(element, className) {
-	var classes = element.className.split(/\s+/),
-		length = classes.length,
-		i = 0;
-
-	for(; i < length; i++) {
-		if (classes[i] === className) {
-			classes.splice(i, 1);
-			break;
+		var classes = element.className.split(/\s+/),
+			length = classes.length,
+			i = 0;
+		for(; i < length; i++) {
+			if (classes[i] === className) {
+				classes.splice(i, 1);
+				break;
+			}
 		}
-	}
-	// The className is not found
-	if (length === classes.length) {
-		classes.push(className);
-	}
+		// The className is not found
+		if (length === classes.length) {
+			classes.push(className);
+		}
 
-	element.className = classes.join(' ');
+		element.className = classes.join(' ');
 	}
 
 	menuLink.onclick = function (e) {
--- starboard.js
+++ starboard.js
@@ -3,40 +3,43 @@ Locations = new Meteor.Collection('locations');
 People = new Meteor.Collection('people');
 
 if (Meteor.isClient) {
-	Session.setDefault("enable_edit",false)
+	Session.setDefault("selectedItem","")
 	
 	Template.OrganizationsList.helpers({
 		organization: function(){
-			return Organizations.find().fetch();
-		}
-	});
-
-	Template.LocationsList.helpers({
-		location: function(){
-			return Locations.find().fetch();
+			return Organizations.findOne();
+		},
+		locationInOrg: function(){
+			return Locations.find({organization_id: this._id}).fetch();
+		},
+		selected: function(){
+			return Session.equals("selectedItem", this._id)?"pure-menu-select":"pure-menu-select";
 		}
 	});
 
 	Template.OrganizationsList.events({
-		'submit form': function(e, tmpl){
+		'click a': function(e, tmpl){
+			console.log(this._id)
+			Session.set("selectedItem", this._id);
+			// e.currentTarget.addClass('pure-menu-selected');
+		},
+		'submit form#addOrg': function(e, tmpl){
 			e.preventDefault();
 			
 			var org_name = tmpl.find('input').value;
 			
-			Organizations.insert({organization_name: org_name, created_at: new Date});
+			Organizations.insert({organization_name: org_name, created_at: new Date, created_by: Meteor.userId()});
 			
 			var form = tmpl.find('form');
 			form.reset();
-		}
-	});
-
-	Template.LocationsList.events({
-		'submit form': function(e, tmpl){
+		},
+		'submit form#addLoc': function(e, tmpl){
 			e.preventDefault();
 			
 			var loc_name = tmpl.find('input').value;
+			var current_org_id = tmpl.find('li.selected_org')
 			
-			Locations.insert({location_name: loc_name, created_at: new Date});
+			Locations.insert({location_name: loc_name, organization_id: org_id, created_at: new Date, created_by: Meteor.userId()});
 			
 			var form = tmpl.find('form');
 			form.reset();
